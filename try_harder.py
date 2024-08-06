e48031849c80429a8da8fe2e36a0542d00de81d7
Justin Woodum <justin.woodum@gmail.com>
2024-08-06 12:24:06 -0700

    Create try_harder.py

---
+++ try_harder.py
@@ -0,0 +1,8 @@
+CHALLENGE
+GPT can be somewhat "lazy" when it researches a topic. It seems to often default to the most official page it can find for a target topic or organization, and then relies on that source to answer questions - it doesn't look for other pages such as social media pages or third-party websites where more information may be available. Instead, GPT just says it can't find the needed information.
+
+SOLUTION
+Sometimes prompts can be engineered to improve GPT's initial research but when nothing seems to work, there is usually one thing that does: asking it to try again or "try harder". This often leads to additional information being found without any additional prompting other than this single phrase!
+
+PROMPTS
+* Try harder.
